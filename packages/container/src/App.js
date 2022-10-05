1c4b05d92cc17ced86e5364236582252bc45f72a
Vinay Kumar <vinkumar30@WKMZT7FC1A16.global.publicisgroupe.net>
2022-10-05 15:41:12 +0530

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MarketingContainer from "./components/MarketingApp";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingContainer />
+    </div>
+  );
+};
+
+export default App;
---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Mount function to start the app
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+/* Scenario 1 - Development mode - In Isolation
+If we are in development and in isolation, call mount immediately.
+*/
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+/*Scenario 2 - Running inside container
+When we are running through container, export the mount function
+*/
+export { mount };
