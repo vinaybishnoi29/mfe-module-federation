5da31639d3ea1e601676de8a0404a232d692df15
Vinay Kumar <vinayrahar1991@gmail.com>
2022-10-05 16:59:22 +0530

    container deployment script

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+      // shared: ["react", "react-dom"],
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+// In merge function, the 2nd parameter overrides any similar options which are already declared.
+module.exports = merge(commonConfig, prodConfig);
---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
+      },
+      // shared: ['react', 'react-dom']
+      // In case you want to make all dependencies to be shared
+      shared: packageJson.dependencies
+    })
+  ]
+};
+
+// In merge function, the 2nd parameter overrides any similar options which are already declared.
+module.exports = merge(commonConfig, prodConfig);
